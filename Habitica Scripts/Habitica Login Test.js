09d1ddafa1cd86cc827663833d6529aaa25265eb
LuisFer <Tom@s2019>
2020-09-13 20:57:31 -0500

    se agregan mas casos de prueba

--- Habitica Scripts/Habitica Login Test.js
+++ Habitica Scripts/Habitica Login Test.js
@@ -11,7 +11,33 @@ context('Habitica Tests', () => {
     cy.get('.btn-info[type="submit"]').click()
     cy.wait(3000)
     cy.get('.svg-icon').should('be.visible')
-    cy.get('.header-welcome').should('contain', ' Welcome back! ')
+  })
+
+  it('logout session', () => {
+    cy.wait(1000)
+    cy.get('.desktop-only > .item-user > .habitica-menu-dropdown-toggle > :nth-child(1) > [aria-label="User"] > .top-menu-icon > svg').click({ force: true })
+    cy.get('.desktop-only > .item-user > .dropdown-menu > .user-dropdown > :nth-child(9)').click()
+  })
+
+  it('makes a unsuccesful login bad password ', () => {
+    cy.visit('https://habitica.com/static/home')
+    cy.wait(3000)
+    cy.get('.login-button').click();
+    cy.wait(3000)
+    cy.get('#usernameInput').type('pa_test');
+    cy.get('#passwordInput').type('fake@email.comm');
+    cy.get('.btn-info[type="submit"]').click()
+    cy.wait(3000)
+    cy.get('.text > div').should('contain', 'password is incorrect')
+  })
+
+  it('makes a unsuccesful login empty user and password ', () => {
+    cy.visit('https://habitica.com/static/home')
+    cy.wait(5000)
+    cy.get('.login-button').click();
+    cy.get('.btn-info[type="submit"]').click()
+    cy.wait(3000)
+    cy.get('.notification').should('contain', 'Missing')
   })
 
 });
--- Habitica Scripts/Habitica SignUp Test.js
+++ Habitica Scripts/Habitica SignUp Test.js
@@ -30,6 +30,16 @@ context('Habitica Tests', () => {
     cy.get('.input-error').should('contain', ' Usernames must be between 1 and 20 characters. ')
   })
 
+  it('makes a sign up attemp with password missmatch', () => {
+    cy.visit('https://habitica.com/static/home')
+    cy.wait(3000)
+    cy.get('input[placeholder="Username"]').type('pa_test3')
+    cy.get('[type="email"]').type('onassottuse-0220@yopmail.com')
+    cy.get('[placeholder="Password"]').type('fake@email.com')
+    cy.get('[placeholder="Confirm Password"]').type('fake@email.commmmm')
+    cy.get('.input-error').should('contain', 'match password.')
+  })
+
   it('makes a sign up attemp with password over 20 characters', () => {
     cy.visit('https://habitica.com/static/home')
     cy.wait(3000)
@@ -38,9 +48,20 @@ context('Habitica Tests', () => {
     cy.get('[placeholder="Password"]').type('estepasswordessuperiora20caracteres12345678910estepasswordessuperiora20caracteres12345678910estepasswordessuperiora20caracteres12345678910estepasswordessuperiora20caracteres12345678910estepasswordessuperiora20caracteres12345678910estepasswordessuperiora20caracteres12345678910estepasswordessuperiora20caracteres12345678910estepasswordessuperiora20caracteres12345678910estepasswordessuperiora20caracteres12345678910estepasswordessuperiora20caracteres12345678910estepasswordessuperiora20caracteres12345678910estepasswordessuperiora20caracteres12345678910');
     cy.get('[placeholder="Confirm Password"]').type('estepasswordessuperiora20caracteres12345678910estepasswordessuperiora20caracteres12345678910estepasswordessuperiora20caracteres12345678910estepasswordessuperiora20caracteres12345678910estepasswordessuperiora20caracteres12345678910estepasswordessuperiora20caracteres12345678910estepasswordessuperiora20caracteres12345678910estepasswordessuperiora20caracteres12345678910estepasswordessuperiora20caracteres12345678910estepasswordessuperiora20caracteres12345678910estepasswordessuperiora20caracteres12345678910estepasswordessuperiora20caracteres12345678910');
     cy.get('.btn-info[type="submit"]').click()
-
-    //cy.get('.input-error').should('contain', ' Usernames must be between 1 and 20 characters. ')
+    cy.get('.row > .btn').click()
+    cy.get('.next-arrow > svg > path').click()
+    cy.get('.next-arrow > svg > path').click()
+    cy.get('.introjs-tooltipbuttons > .btn').click()
   })
 
-
+  it('delete an account', () => {
+    cy.wait(3000)
+    cy.get('.desktop-only > .item-user > .habitica-menu-dropdown-toggle > :nth-child(1) > [aria-label="User"] > .top-menu-icon > svg').click()
+    cy.wait(3000)
+    cy.get('.desktop-only > .item-user > .dropdown-menu > .user-dropdown > [href="/user/settings/site"]').click()
+    cy.wait(3000)
+    cy.get('.panel-body > :nth-child(3) > div > :nth-child(2)').click()
+    cy.get('.col-6 > .form-control').type('estepasswordessuperiora20caracteres12345678910estepasswordessuperiora20caracteres12345678910estepasswordessuperiora20caracteres12345678910estepasswordessuperiora20caracteres12345678910estepasswordessuperiora20caracteres12345678910estepasswordessuperiora20caracteres12345678910estepasswordessuperiora20caracteres12345678910estepasswordessuperiora20caracteres12345678910estepasswordessuperiora20caracteres12345678910estepasswordessuperiora20caracteres12345678910estepasswordessuperiora20caracteres12345678910estepasswordessuperiora20caracteres12345678910')
+    cy.get('.modal-footer > .btn-danger').click()
+  })
 });
