86aa843310a22a6f9d3e4e40c33cd381d5862994
Gabriel Pinto <gl.pinto10@uniandes.edu.co>
2020-09-26 00:50:38 -0500

    Version is loaded on mobile

--- App/logic/MobileAppLogic.js
+++ App/logic/MobileAppLogic.js
@@ -1,17 +1,33 @@
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
+const path = require('path');
+const WebAppConfigPersistence = require('../persistence/WebAppConfigPersistence');
+const VersionsPersistence = require('../persistence/VersionsPersistence');
 
 /*
 Método encargado de obtener todas las apps web
 */
-module.exports.execMobileAppConfig = async (id_config) => {
+module.exports.execMobileAppConfig = async (id) => {
     try {
-        //Se ejecuta el comando en consola
-        const { stdout, stderr } = await exec('sh ~/Documents/Monkey_Wiki.sh');
-        console.log('stdout:', stdout);
-        if (stderr !== '') {
-            console.log('stderr:', stderr);
+        const response = await WebAppConfigPersistence.fetchWebAppVersionConfig(id);
+        console.log(response);
+        if (response === null) {
+            const errJson = {
+                errMsg: 'No existe una configuracion con este id',
+                errCode: 400,
+            };
+            errJson.error = new Error();
+            throw errJson;
+        } else {
+            //Se ejecuta el comando en consola
+            const currentVersion = await VersionsPersistence.fetchVersion(response.pruebas[0][0]);
+            const { stdout, stderr } = await exec('sh ~/Documents/Monkey_Wiki.sh');
+            console.log('stdout:', stdout);
+            if (stderr !== '') {
+                console.log('stderr:', stderr);
+            }
         }
+
     } catch (err) {
         const errJson = {
             error: new Error(),
