9aaf615ef450e341a670a72526577253a810180d
andresvaron <af.varon@uniandes.edu.co>
2020-11-07 09:52:30 -0500

    Correciones index

--- App/routes/index.js
+++ App/routes/index.js
@@ -3,7 +3,6 @@ const router = express.Router();
 
 router.use('/pruebas', require('./PruebasIndex'));
 router.use('/web', require('./WebAppIndex'));
-router.use('/mobile', require('./MobileAppIndex'));
 router.use('/testResults', require('./TestsResultsAPI'));
 router.use('/vrtResults', require('./VRTResultsAPI'));
 router.use('/tests', require('./TestsAPI'));
